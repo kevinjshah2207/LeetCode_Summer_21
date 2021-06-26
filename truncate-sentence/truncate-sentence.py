b05a8b7252ce25b4bb2156d4733f0d1510f73353
Kevin Shah <kevinjshah2207@gmail.com>
2021-06-25 19:42:12 -0700

    Time: 32 ms (56.15%), Space: 14 MB (98.74%) - LeetHub

--- truncate-sentence/truncate-sentence.py
+++ truncate-sentence/truncate-sentence.py
@@ -2,14 +2,20 @@ class Solution:
     def truncateSentence(self, s: str, k: int) -> str:
         i = 0
         n = len(s)
-        while(k>0 and i<n):
-            if s[i] == " ":
+        # while(k>0 and i<n):
+        #     if s[i] == " ":
+        #         k -= 1
+        #         i += 1
+        #     else:
+        #         i += 1
+        # if i >= n:
+        #     return s
+        # else:
+        #     return s[:i-1]
+        for i,c in enumerate(s):
+            if c == ' ':
                 k -= 1
-                i += 1
-            else:
-                i += 1
-        if i >= n:
-            return s
-        else:
-            return s[:i-1]
+                if k == 0:
+                    return s[:i]
+        return s
             
