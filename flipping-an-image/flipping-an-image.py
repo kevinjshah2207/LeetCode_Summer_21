b116b8f3d83c65c0f891f8cbac1496a15c686348
Kevin Shah <kevinjshah2207@gmail.com>
2021-06-25 19:26:57 -0700

    Time: 44 ms (93.80%), Space: 14.3 MB (52.35%) - LeetHub

--- flipping-an-image/flipping-an-image.py
+++ flipping-an-image/flipping-an-image.py
@@ -4,8 +4,5 @@ class Solution:
             image[i].reverse()
         for i in range(len(image)):
             for j in range(len(image[0])):
-                if image[i][j] == 1:
-                    image[i][j] = 0
-                else:
-                    image[i][j] = 1
+                image[i][j] ^= 1
         return image
