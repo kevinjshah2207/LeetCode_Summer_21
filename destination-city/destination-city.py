ece492459bc9d77c06d7cc19828f1bc96dbe3b60
Kevin Shah <kevinjshah2207@gmail.com>
2021-07-03 16:23:45 -0700

    Time: 52 ms (77.07%), Space: 14.5 MB (15.30%) - LeetHub

--- destination-city/destination-city.py
+++ destination-city/destination-city.py
@@ -1,19 +1,20 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        if len(paths) == 1:
-            return paths[0][1]
-        d = {}
-        for i in range(len(paths)):
-            if paths[i][1] in d:
-                d[paths[i][1]] -= 1
-            else:
-                d[paths[i][1]] = 1
+        return (set([path[1] for path in paths]) - set([path[0] for path in paths])).pop()
+#         if len(paths) == 1:
+#             return paths[0][1]
+#         d = {}
+#         for i in range(len(paths)):
+#             if paths[i][1] in d:
+#                 d[paths[i][1]] -= 1
+#             else:
+#                 d[paths[i][1]] = 1
         
-            if paths[i][0] in d:
-                d[paths[i][0]] -= 1
-            else:
-                d[paths[i][0]] = 0
-        for key, value in d.items():
-            if value == 1:
-                return key
+#             if paths[i][0] in d:
+#                 d[paths[i][0]] -= 1
+#             else:
+#                 d[paths[i][0]] = 0
+#         for key, value in d.items():
+#             if value == 1:
+#                 return key
             
