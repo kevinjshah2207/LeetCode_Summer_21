326094956a337d643ee85690a621f349d8d5db70
Kevin Shah <kevinjshah2207@gmail.com>
2021-07-04 19:22:45 -0700

    Time: 124 ms (33.33%), Space: 14.4 MB (100.00%) - LeetHub

--- build-array-from-permutation/build-array-from-permutation.py
+++ build-array-from-permutation/build-array-from-permutation.py
@@ -1,8 +1,11 @@
+#https://helloml.org/build-array-from-permutation-solution-to-leetcode-problem/
+
+
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
         n = len(nums)
         for i in range(len(nums)):
-            nums[i] = nums[i] + (n*(nums[nums[i]]%n))
+            nums[i] = nums[i] + (n*(nums[nums[i]]%n)) #Modulo operator is the answer to this problem
             
         for i in range(len(nums)):
             nums[i] = nums[i] // n
