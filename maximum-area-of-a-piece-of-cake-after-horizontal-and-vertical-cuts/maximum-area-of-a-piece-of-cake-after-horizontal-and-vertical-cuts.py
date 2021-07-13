ca2a795075478af420b2793b8e00eda9c7025bc4
Kevin Shah <kevinjshah2207@gmail.com>
2021-07-12 23:11:12 -0700

    Time: 364 ms (44.24%), Space: 27 MB (70.25%) - LeetHub

--- maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -2,12 +2,15 @@ class Solution:
     def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
         horizontalCuts.sort()
         verticalCuts.sort()
-        horizontalCuts.insert(0,0)
-        verticalCuts.insert(0,0)
-        horizontalCuts.append(h)
-        verticalCuts.append(w)
+        horizontalCuts = [0] + horizontalCuts + [h]
+        verticalCuts = [0] + verticalCuts + [w]
+        # horizontalCuts.insert(0,0)
+        # verticalCuts.insert(0,0)
+        # horizontalCuts.append(h)
+        # verticalCuts.append(w)
         height = 0
         length = 0
+        # mod = 10000007
         for i in range(len(horizontalCuts)-1):
             height = max(height, horizontalCuts[i+1]-horizontalCuts[i])
         for i in range(len(verticalCuts)-1):
