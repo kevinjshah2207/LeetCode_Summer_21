65062f5f8c5e1b22012bc06da4473e45f025fdd0
Kevin Shah <kevinjshah2207@gmail.com>
2021-07-19 19:52:23 -0700

    Time: 32 ms (65.64%), Space: 14.1 MB (90.70%) - LeetHub

--- invert-binary-tree/invert-binary-tree.py
+++ invert-binary-tree/invert-binary-tree.py
@@ -6,6 +6,27 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
-        if root:
-            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
-            return root
+        
+        #Recursive Solution
+        # if root:
+        #     root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        #     return root
+        
+        # BFS Solution
+        queue = collections.deque([(root)])
+        while queue:
+            node = queue.popleft()
+            if node:
+                node.left, node.right = node.right, node.left
+                queue.append(node.left)
+                queue.append(node.right)
+        return root
+        
+        #DFS Soluction
+        # stack = [root]
+        # while stack:
+        #     node = stack.pop()
+        #     if node:
+        #         node.left, node.right = node.right, node.left
+        #         stack.extend([node.right, node.left])
+        # return root
