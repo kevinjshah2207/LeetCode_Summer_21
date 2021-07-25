7f37bf803aefd50e2bb9399433c07a92f7121f27
Kevin Shah <kevinjshah2207@gmail.com>
2021-07-24 20:03:27 -0700

    Time: 1401 ms (40.91%), Space: 17.3 MB (86.28%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -1,3 +1,10 @@
+# Sort the array. As the sum of 3 numbers is 0, take one number and multiply it by -1.
+# which becomes the target
+# As nums[i] + nums[j] + nums[k] = 0, nums[i] + nums[j] = -nums[k]. Now we just have to
+# find 2 numbers after the target whose sum is equal to the negative of the target. 
+# During each iteration, we also get rid of duplicates by continuing numbers until
+# nums[s] == nums[s-1]. This gives rise to a O(n^2) algorithm for the 3 Sum problem.
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         if not nums or len(nums) < 3:
