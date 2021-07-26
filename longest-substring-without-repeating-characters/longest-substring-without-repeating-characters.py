62108307764445ddd7e007b4638bd384ea00a891
Kevin Shah <kevinjshah2207@gmail.com>
2021-07-25 18:21:18 -0700

    Time: 56 ms (84.91%), Space: 14.6 MB (6.77%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,3 +1,10 @@
+# Sliding Window Approach: We create a dictionary that stores the index of occurrence of
+# the character. We keep calculating the length of the substring in each iteration.
+# If the character is already in the dict, we check if its occurrence is after our 
+# sliding window's start position (l) or not. If it is not present in the current window
+# then we just normally calculate the length, else, we slide the window start position to
+# the right of where the repeated character was first seen.
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         if not s:
