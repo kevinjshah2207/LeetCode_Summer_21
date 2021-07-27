eded4f2226b7c1bdb54c96d8607c4175f13acca4
Kevin Shah <kevinjshah2207@gmail.com>
2021-07-26 22:59:23 -0700

    Time: 32 ms (68.24%), Space: 14.4 MB (16.00%) - LeetHub

--- minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.py
+++ minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.py
@@ -1,3 +1,7 @@
+# We just have to find out the parity of each coin's position. As changing parity costs 1, we
+# just have to find out the minimum number of changes required to make all the coins have the
+# same parity.
+
 class Solution:
     def minCostToMoveChips(self, position: List[int]) -> int:
         odd = 0
