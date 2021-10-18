1328d080c88f5c92801fd0bc113c0b2862ea0eb7
Kevin Shah <kevinjshah2207@gmail.com>
2021-10-17 17:31:08 -0700

    Time: 48 ms (80.69%), Space: 14.6 MB (33.66%) - LeetHub

--- largest-unique-number/largest-unique-number.py
+++ largest-unique-number/largest-unique-number.py
@@ -7,8 +7,6 @@ class Solution:
             else:
                 d[val] += 1
         d = dict(sorted(d.items(), key=lambda item: -item[0]))
-        print(d)
-        # d = sorted(d.items(), key=lambda x: -x[1])
         for i in d:
             if d[i] == 1:
                 return i
