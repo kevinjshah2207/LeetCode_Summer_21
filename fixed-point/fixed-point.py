1e6cb4a0ee1cc270c97d8daddd2147c8e485d3da
Kevin Shah <kevinjshah2207@gmail.com>
2021-10-19 22:20:51 -0700

    Time: 90 ms (32.24%), Space: 15.2 MB (84.49%) - LeetHub

--- fixed-point/fixed-point.py
+++ fixed-point/fixed-point.py
@@ -1,9 +1,17 @@
 class Solution:
     def fixedPoint(self, arr: List[int]) -> int:
-        for i, val in enumerate(arr):
-            if val > i:
-                return -1
+        # for i, val in enumerate(arr):
+        #     if val > i:
+        #         return -1
+        #     else:
+        #         if i == val:
+        #             return i
+        # return -1
+        l, r = 0, len(arr)-1
+        while l < r:
+            mid = (l+r)//2
+            if arr[mid] - mid < 0:
+                l = mid+1
             else:
-                if i == val:
-                    return i
-        return -1
+                r = mid
+        return l if arr[l] == l else -1
