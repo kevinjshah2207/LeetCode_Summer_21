06759b5af5536b0c943f8737687b4f2112a1acba
Kevin Shah <kevinjshah2207@gmail.com>
2021-10-31 16:42:48 -0700

    Time: 244 ms (71.56%), Space: 19 MB (91.25%) - LeetHub

--- clone-binary-tree-with-random-pointer/clone-binary-tree-with-random-pointer.py
+++ clone-binary-tree-with-random-pointer/clone-binary-tree-with-random-pointer.py
@@ -5,19 +5,39 @@
 #         self.left = left
 #         self.right = right
 #         self.random = random
-
+import collections
 class Solution:
-    def __init__(self):
-        self.visited = dict()
+    # def __init__(self):
+    #     self.visited = dict()
     def copyRandomBinaryTree(self, root: 'Node') -> 'NodeCopy':
+        ##### Iterative DFS
         if not root:
             return None
-        if root in self.visited:
-            return self.visited[root]
-        node = NodeCopy(root.val, None, None, None)
-        self.visited[root] = node
+        visited = collections.defaultdict(lambda: NodeCopy())
+        visited[None] = None
         
-        node.left = self.copyRandomBinaryTree(root.left)
-        node.right = self.copyRandomBinaryTree(root.right)
-        node.random = self.copyRandomBinaryTree(root.random)
-        return node
+        S = [root]
+        while S:
+            node = S.pop()
+            visited[node].val = node.val
+            visited[node].left = visited[node.left]
+            visited[node].right = visited[node.right]
+            visited[node].random = visited[node.random]
+            for child in (node.left, node.right):
+                if child:
+                    S.append(child)
+        return visited[root]
+            
+        ##### Recursive DFS
+#         if not root:
+#             return None
+#         if root in self.visited:
+#             return self.visited[root]
+#         node = NodeCopy(root.val, None, None, None)
+#         self.visited[root] = node
+        
+#         node.left = self.copyRandomBinaryTree(root.left)
+#         node.right = self.copyRandomBinaryTree(root.right)
+#         node.random = self.copyRandomBinaryTree(root.random)
+#         return node
+
